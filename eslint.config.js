88ac2af601ef87accae6ffcdb59635ca46be60fd
Florencea <bearflorencea@gmail.com>
2025-10-06 00:48:40 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { globalIgnores } from "eslint/config";
+import { globalIgnores, defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config([
-  globalIgnores(["dist", "public"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2022,
-      globals: globals.browser,
-    },
+export default defineConfig(globalIgnores(["dist", "public"]), {
+  files: ["**/*.{ts,tsx}"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs["recommended-latest"],
+    reactRefresh.configs.vite,
+  ],
+  languageOptions: {
+    ecmaVersion: 2022,
+    globals: globals.browser,
   },
-]);
+});
